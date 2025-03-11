38dddbd641ee9ad219b891bf59855fec60c5c822
leonardo <ivanpachecoleo23@gmail.com>
2025-03-11 16:08:46 -0500

    build start

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Include the hotels app's URLs
-    path('', include('hotels.urls')),
-]
--- hotels/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Hotel, Room, Booking
-
-models = [Hotel, Room, Booking]
-
-# Register your models here.
-admin.site.register(models)
