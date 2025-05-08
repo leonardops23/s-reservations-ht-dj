6bf13a50b9f17c75ae70c93df2facc75b930a7f8
Leonardo <ivanpachecoleo23@gmail.com>
2025-05-08 10:00:55 -0500

    add init start

--- store/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Categoria(models.Model):
-    pass
-
-
-class Producto(models.Model):
-    pass
--- store/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    hello = "Hello, World!"
-    context = {
-        'hello': hello,
-    }
-    return render(request, 'home.html', context)
