4bac006a4957344114da1574d03ef641e995a64d
Leonardo Pacheco <ivanpachecoleo23@gmail.com>
2025-08-26 16:09:34 -0500

    Revert "sematic type"

--- sleenpingIn/app/components/forms/SelectCountry.tsx
+++ sleenpingIn/app/components/forms/SelectCountry.tsx
@@ -4,29 +4,30 @@ import Select from "react-select";
 import useCountries from "@/app/components/hooks/useCountries";
 
 export type SelectCountryValue = {
-  label: string;
-  value: string;
+    label: string;
+    value: string;
 }
 
 interface SelectCountryProps {
-  value: SelectCountryValue;
-  onChange: (value: SelectCountryValue) => void;
+    value: SelectCountryValue;
+    onChange: (value: SelectCountryValue) => void;
 }
 
 const SelectCountry: React.FC<SelectCountryProps> = ({
-  value,
-  onChange
+    value,
+    onChange
 }) => {
-  const { getAll } = useCountries();
-  return (
-    <div>
-      <Select
-        placeholder="Anywhere"
-        options={getAll()}
-        isSearchable
-      />
-    </div>
-  )
+    const { getAll } = useCountries();
+    return (
+        <div>
+            <Select
+                placeholder="Anywhere"
+                options={getAll()}
+                isSearchable
+                
+            />
+        </div>
+    )
 }
 
 export default SelectCountry
